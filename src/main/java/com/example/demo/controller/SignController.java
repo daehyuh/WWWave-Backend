668f28bcac6d8bab594deead06ce332e83e10c92
daehy <rkdeown10@naver.com>
2024-01-27 22:00:04 +0900

    feat: use Authentication

--- src/main/java/com/example/demo/controller/SignController.java
+++ src/main/java/com/example/demo/controller/SignController.java
@@ -2,24 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.SignRequest;
 import com.example.demo.dto.SignResponse;
-import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.SignService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class SignController {
 
-    private final MemberRepository memberRepository;
     private final SignService memberService;
 
     @Operation(operationId = "login", summary = "로그인", description = "요청을 검토한뒤 로그인", tags = "SignController")
@@ -36,24 +30,24 @@ public class SignController {
 
     @Operation(operationId = "myinfo", summary = "내정보 보기", description = "요청을 검토한뒤 회원수정", tags = "SignController")
     @GetMapping("/myinfo")
-    public ResponseEntity<SignResponse> myinfo(Principal principal) throws Exception {
-        System.out.println(principal.getName());
-        return new ResponseEntity<>( memberService.getMember(principal.getName()), HttpStatus.OK);
+    public ResponseEntity<SignResponse> myinfo(Authentication authentication) throws Exception {
+        System.out.println(authentication.getName());
+        return new ResponseEntity<>( memberService.getMember(authentication.getName()), HttpStatus.OK);
     }
 
     @Operation(operationId = "update", summary = "닉네임 수정", description = "요청을 검토한뒤 회원수정", tags = "SignController")
     @PostMapping("/updateNick")
-    public ResponseEntity<Boolean> updateNick(@RequestBody SignRequest request, Principal principal) throws Exception {
-        System.out.println(principal.getName());
-        return new ResponseEntity<Boolean>(memberService.updateNickname(request.getNickname(), principal.getName()), HttpStatus.OK);
+    public ResponseEntity<Boolean> updateNick(@RequestBody SignRequest request, Authentication authentication) throws Exception {
+        System.out.println(authentication.getName());
+        return new ResponseEntity<Boolean>(memberService.updateNickname(request.getNickname(), authentication.getName()), HttpStatus.OK);
     }
 
     // 자기소개, 태그 수정
     @Operation(operationId = "update", summary = "자기소개, 태그 수정", description = "요청을 검토한뒤 회원수정", tags = "SignController")
     @PostMapping("/updateMyself")
-    public ResponseEntity<Boolean> update(@RequestBody SignRequest request, Principal principal) throws Exception {
-        System.out.println(principal.getName());
-        return new ResponseEntity<Boolean>(memberService.updateMyselfAndHashtag(request.getHashtag(), request.getHashtag(), principal.getName()), HttpStatus.OK);
+    public ResponseEntity<Boolean> update(@RequestBody SignRequest request, Authentication authentication) throws Exception {
+        System.out.println(authentication.getName());
+        return new ResponseEntity<Boolean>(memberService.updateMyselfAndHashtag(request.getHashtag(), request.getHashtag(), authentication.getName()), HttpStatus.OK);
     }
 
 }
